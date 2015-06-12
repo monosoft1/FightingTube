24f4b45109af55b6ef1138675f0cd20b55a91a50
Hamza Abdullah <monosoft54@gmail.com>
2015-06-12 11:33:34 +0300

    First
    1st

---
+++ FightingTube/Assets/Scripts/UI 1/BlueTeam/BlueTeamButtonFunction.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var TSBox : GameObject;
+
+function BTFunction () 
+{
+     if(SpawnManager.CurTeam == "")
+     {
+        SpawnManager.TSCanvasO.SetActive(false);
+        Network.Instantiate(SpawnManager.BluePlayer, SpawnManager.SpawnPointBlue.transform.position, GameObject.FindGameObjectWithTag("SpawnM").transform.rotation, 0);
+        SpawnManager.CurTeam = "Blue";
+        SpawnManager.Dead = false;
+        SpawnManager.PlayerStatus.SetActive(true);
+     }
+}
---
+++ FightingTube/Assets/Scripts/UI 1/Disconnect/Scripts/DisconnectBFS.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Clicked () 
+{
+  if(SpawnManager.Connection == true)
+  {
+        Network.Disconnect(200);
+        
+        SpawnManager.NDisconnectB.SetActive(true);
+        SpawnManager.Connection = false;
+        SpawnManager.Dead = true;
+  }
+}
