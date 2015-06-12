a10fe1a5f5ec240c8a6a475befd10691c54ed4f7
Hamza Abdullah <monosoft54@gmail.com>
2015-06-13 00:52:50 +0300

    Third
    3rd

--- FightingTube/Assets/Scripts/UI 1/RemoteIP/ValueChanged.js
+++
@@ -1,7 +0,0 @@
-﻿#pragma strict
-
-
-function Changed () 
-{
-   DirectConnection.remoteIPadress = GameObject.FindGameObjectWithTag("RemoteIPT").GetComponent.<UnityEngine.UI.InputField>().text;  
-}
